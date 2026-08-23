5bb0cc05f32582051b7a106c082ae5f7c3af1b6d
ntgiang <giangk9a1@gmail.com>
2026-08-23 14:28:23 +0700

    feat: public category API

---
+++ internal/delivery/category.delivery.go
@@ -0,0 +1,19 @@
+package delivery
+
+import (
+	"lms-backend/internal/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+type categoryDelivery struct {
+	categoryHandler *handler.CategoryHandler
+}
+
+func NewCategoryDelivery(categoryHandler *handler.CategoryHandler) *categoryDelivery {
+	return &categoryDelivery{categoryHandler: categoryHandler}
+}
+
+func (d *categoryDelivery) RegisterRouter(apiGroup *gin.RouterGroup) {
+	apiGroup.GET("categories", d.categoryHandler.FindAll)
+}
